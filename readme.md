05d5eba643fc6fd8a067d94f32665f43dca4be73
Fizal Sarif <fizal.sarif@gmail.com>
2020-03-31 03:07:58 +0000

    update readme and fixed up bot for repl.it

--- readme.md
+++ readme.md
@@ -4,11 +4,25 @@ Discord bot written in Java running on repl.it. Hopefully we can get this up and
 
 ## Dependencies
 
-Create a .env file in your repl.it from this repo with the following: 
+Create a .env file in your repl.it (if you forked this) with the following: 
 
 ```
 DISCORD_TOKEN=<your bot's token>
 ```
 
+## How to Edit
 
-TODO: ADD more stuff here.
+This project consists of 2 parts: 
+
+- A spring boot webserver to make sure repl.it keeps our bot alive. This consists of two java files (`MainApplication.java` & `WebController.java`) and a butt ton of maven packages.
+- A discord bot (`Bot.java`) which already has the `onMessageReceived` event handler set up.
+
+Because java is dumb these files are found in the `src/main/java/me/fizal/MerlinBot` folder. If you need to make any more classes, add them to that folder and make sure to add `package me.fizal.MerlinBot;` to the top of your file. 
+
+## WTF - Why is there a webserver here?
+
+In order for repl.it to keep our bot alive and not kill it when we close replit. There's a monitor on https://uptimerobot.com/ to ping this repl.it's web url (https://MerlinBot--fizal619.repl.co) every 58 minute. Hopefully this keeps it alive. 
+
+## Resources: 
+
+- [JDA (Java Discord API)](https://github.com/DV8FromTheWorld/JDA)
--- src/main/java/me/fizal/MerlinBot/Bot.java
+++ src/main/java/me/fizal/MerlinBot/Bot.java
@@ -13,10 +13,15 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class Bot extends ListenerAdapter {
 
-  static Dotenv dotenv = Dotenv.load();
-  static String API_KEY = dotenv.get("DISCORD_TOKEN");
+  static String API_KEY;
 
   public Bot main() throws LoginException {
+    try {
+      Dotenv dotenv = Dotenv.load();
+      API_KEY = dotenv.get("DISCORD_TOKEN");
+    } catch (Exception e) {
+      API_KEY = System.getenv("DISCORD_TOKEN");
+    }
     new JDABuilder(API_KEY)
     .addEventListeners(new Bot())
     .setActivity(Activity.playing("Type .merlin"))
@@ -25,18 +30,26 @@ public class Bot extends ListenerAdapter {
     return this;
   }
 
+  // do all your own java down here, do not modify above. 
+
   @Override
   public void onMessageReceived(MessageReceivedEvent event) {
+    // all your codes within this function restart the bot on code changes
+
     Message msg = event.getMessage();
 
     System.out.println("\n");
     System.out.println(msg.getContentRaw());
     System.out.println("\n");
 
-    if (msg.getContentRaw().contains(".merlin")) {
+    //maybe this should be help text
+    if (msg.getContentRaw().equals(".merlin")) {
       MessageChannel channel = event.getChannel();
-      channel.sendMessage("Hoh skunt, meh ah live bai!")
+      channel.sendMessage("Hoh skunt, meh ah live bai! 🔮🧙‍♀️")
         .queue();
     }
+  
+  
+  // end of safezone
   }
 }
